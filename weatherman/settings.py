d5299ee539262bdc7ce405b47aad2088d4f72e54
Abdulazeezjr <129750361+Abdulazeezjr@users.noreply.github.com>
2023-12-24 15:22:52 +0100

    new changes

--- weatherman/settings.py
+++ weatherman/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-+-afffm646)9_b6y5qr!n^pt_2*h3vk9@&x*px#ag=+7)^=r!v
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
